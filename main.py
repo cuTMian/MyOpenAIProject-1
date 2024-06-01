6f3307debf334d2217aadc405adc709dbd6185e3
cuTMian <wanwan_625273669@qq.com>
2024-06-01 12:27:19 +0800

    MOAP-1-start add main.py and basic function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from openai import OpenAI
 
 client = OpenAI()
+
+model = "gpt-3.5-turbo"
+msg = [{"role":"user", "content":"三国演义讲了一个什么故事"}]
+
+response = client.chat.completions.create(
+    model=model,
+    messages=msg
+)
+
+print(response.choices[0].message.content)
